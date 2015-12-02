f7fb114aa5557ad61d599e189156adedefe7fa3c
Andrea Casaccia <acasaccia@gmail.com>
2015-12-02 14:59:31 +0000

    Update australian-voting.cpp

--- 01-introduction-to-algorithm-design/challenges/110108-australian-voting/australian-voting.cpp
+++ 01-introduction-to-algorithm-design/challenges/110108-australian-voting/australian-voting.cpp
@@ -3,48 +3,96 @@
  */
 
 #include <iostream>
+#include <string>
 #include <sstream>
+
+void parse_case(int& candidates_count, int& votes_count, char* candidates, int** votes) {
+	
+	std::string line;
+	std::stringstream ss;
+	
+	std::cin >> candidates_count;
+	std::cin.get();
+
+	for (int j=0; j<candidates_count; j++) {
+		std::cin.getline(candidates[j], sizeof(candidates[j]), '\n');
+	}
+
+	votes_count = 0;
+	
+	while (std::getline(std::cin, line)) {
+		if (line.empty()) {
+			break;
+		}
+		ss.str(line);
+		for (int k=0; k<candidates_count; k++) {
+			ss >> votes[k][votes_count];
+		}
+		std::cin.get();
+		votes_count++;
+	}
+	
+}
+
+void count_votes(int candidates_count, int votes_count, int* candidate_votes, int** votes) {
+	
+	for (int j=0; j<candidates_count; j++) {
+		candidate_votes[j] = 0;
+	}
+	
+	for (int k=0; k<candidates_count; k++) {
+		for (int j=0; j<votes_count; j++) {
+			candidate_votes[votes[k][j]-1]++;
+		}
+	}
+}
+
+int find_winners() {
+	
+}
+
+int find_losers() {
+	
+}
+
+int eliminate_losers() {
+	
+}
 
 int main( int argc, char * argv[] )
 {
-
-	int cases, candidates_number;
+	int cases_count, candidates_count, votes_count;
 	char candidates[20][80];
 	int votes[20][1000];
-	int votes_count[20];
+	int candidate_votes[20];
+	int results[20];
 
-	std::string line;
-
-	std::cin >> cases;
+	std::cin >> cases_count;
 	std::cin.get();
 	std::cin.get();
 
-	for (int i=0; i<cases; i++) {
+	for (int i=0; i<cases_count; i++) {
 
-		std::cin >> candidates_number;
-		std::cin.get();
-
-		for (int j=0; j<candidates_number; j++) {
-			std::cin.getline(candidates[j], sizeof(candidates[j]), '\n');
-		}
-
-		int votes_number = 0;
-
-		while (std::getline(std::cin, line)) {
-			if (line.empty()) {
-				break;
+		parse_case(&candidates_count, &votes_count, candidates, votes);
+		
+		
+		count_votes(candidates_count, votes_count, candidate_votes, votes);
+		if (winners = find_winners(candidate_votes, results) > 0) {
+			for (int j=0; j<winners; j++) {
+				std::cout << candidates[results[j]] << std::endl;
 			}
-			std::cout << line << std::endl;
-			votes_number++;
+			std::cout << std::endl;
+		} else {
+			losers = find_losers(candidate_votes, results);
+			eliminate_losers(votes, results)
 		}
-
-		for (int j=0; j<20; j++) {
-			votes_count[j] = 0;
-		}
-
-		for (int k=0; k<candidates_number; k++) {
-			for (int j=0; j<votes_number; j++) {
-				votes_count[votes[k][j]-1]++;
+		
+		for (int k=0; k<candidates_count; k++) {
+			if ((double) votes_count[k] / votes_count > 0.5) {
+				std::cout << candidates[k];
+				return 0;
+			} else {
+				
 			}
 		}
 
