9318e7ef15c4d8ba2da52fdb7727f4479f7420f4
Andrea Casaccia <acasaccia@gmail.com>
2016-01-02 17:11:57 +0000

    Heapsort implementation

---
+++ 04-sorting-and-searching/HeapSort.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var PriorityQueue = require("../03-data-structures/PriorityQueue.js");
+
+module.exports = function(array) {
+
+    var pq = new PriorityQueue();
+
+    while (array.length) {
+        pq.insert(array.shift());
+    }
+
+    while (!pq.empty()) {
+        array.push(pq.get());
+    }
+
+    return array;
+
+};
