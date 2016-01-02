022c21f37c618b418263f11c83bf10e4f71bff7a
Andrea Casaccia <acasaccia@gmail.com>
2016-01-02 10:44:26 +0000

    Fixed Dijkstra specs

--- 10-graph-algorithms/spec/ShortestPath.spec.js
+++ 10-graph-algorithms/spec/ShortestPath.spec.js
@@ -9,12 +9,12 @@ describe("Shortest path Dijkstra", function() {
 
     var vertexes = [
         [0, 2],
-        [2, 7],
-        [7, 3],
-        [3, 6]
+        [0, 7],
+        [0, 3],
+        [0, 6]
     ];
 
-    var distances = [ 0.26, 0.34, 0.39, 0.52 ];
+    var distances = [ 0.26, 0.60, 0.99, 1.51 ];
 
     var paths = [
         [0, 2],
@@ -23,17 +23,16 @@ describe("Shortest path Dijkstra", function() {
         [0, 2, 7, 3, 6]
     ];
 
-    for (var i=0; i<vertexes_pair.length; i++) {
-
-        var shortest_path = Dijkstra(graph, vertexes[i][0]);
-
-        it("distance from " + vertexes[i][0] + " to " + vertexes[i][1] + " should be " + distances[i], function(){
-            expect(shortest_path.to(vertexes[i][1]).distance).toBeCloseTo(distances[i], 2);
-        });
-
-        it("path from " + vertexes[i][0] + " to " + vertexes[i][1] + " should be " + distances[i], function(){
-            expect(shortest_path.to(vertexes[i][1]).path).toEqual(paths[i]);
-        });
+    for (var i=0; i<vertexes.length; i++) {
+        (function(i){
+            var shortest_path = new Dijkstra(graph, vertexes[i][0]);
+            it("distance from " + vertexes[i][0] + " to " + vertexes[i][1] + " should be " + distances[i], function(){
+                expect(shortest_path.to(vertexes[i][1]).distance).toBeCloseTo(distances[i], 2);
+            });
+            it("path from " + vertexes[i][0] + " to " + vertexes[i][1] + " should be [ " + paths[i].join(', ') + ' ]', function(){
+                expect(shortest_path.to(vertexes[i][1]).path).toEqual(paths[i]);
+            });
+        })(i);
     }
 
 });
