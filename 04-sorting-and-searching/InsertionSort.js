d16637f3f9284a82a751835ea81fa70b7cf31be9
Andrea Casaccia <acasaccia@gmail.com>
2016-01-02 19:26:33 +0000

    Insertion sort

---
+++ 04-sorting-and-searching/InsertionSort.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = function(array) {
+
+    var j;
+
+    function swap(a, b) {
+        var tmp = array[a];
+        array[a] = array[b];
+        array[b] = tmp;
+    }
+
+    for (var i=1; i<array.length; i++) {
+        j = i;
+        while (j>0 && array[j-1] > array[j]) {
+            swap(j, j-1);
+            j--;
+        }
+    }
+
+    return array;
+
+};
