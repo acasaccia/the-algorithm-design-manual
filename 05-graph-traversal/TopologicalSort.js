54dae2bd38a9913432eb2d44e440f096fe88f284
Andrea Casaccia <acasaccia@gmail.com>
2016-01-09 17:58:26 +0000

    Changed topological sort to work with DirectedWeightedGraph

--- 05-graph-traversal/TopologicalSort.js
+++ 05-graph-traversal/TopologicalSort.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var DirectedEdge = require("../06-weighted-graph-algorithms/DirectedEdge.js");
+
 module.exports = function TopologicalSort(graph) {
 
     var vertexes_count = graph.getVertexesCount();
@@ -14,7 +16,11 @@ module.exports = function TopologicalSort(graph) {
         var adjacents = graph.getAdjacents(n);
         for (var i=0; i<adjacents.length; i++) {
             if (!on_the_stack[adjacents[i]] && !visited[adjacents[i]]) {
-                visit(adjacents[i]);
+                if (adjacents[i] instanceof DirectedEdge) {
+                    visit(adjacents[i].to());
+                } else {
+                    visit(adjacents[i]);
+                }
             }
         }
         post_order.push(n);
