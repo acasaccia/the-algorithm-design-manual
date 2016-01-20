4972603feac9d3a71f58a050d217f75fb70164d2
Andrea Casaccia <acasaccia@gmail.com>
2016-01-20 23:57:17 +0000

    Rabin-Karp c++

---
+++ 04-sorting-and-searching/RabinKarpSearch.h
@@ -0,0 +1,15 @@
+/*
+ * RabinKarpSearch.h
+ *
+ *  Created on: 20 Jan 2016
+ *      Author: andrea
+ */
+
+#ifndef RABINKARPSEARCH_H_
+#define RABINKARPSEARCH_H_
+
+namespace RabinKarp {
+	size_t find (const std::string&, const std::string&, size_t pos = 0);
+}
+
+#endif /* RABINKARPSEARCH_H_ */
