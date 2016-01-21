e95749241e4c804cd4734a4203de5ddcf3b1bcee
acasaccia <acasaccia@gmail.com>
2016-01-21 16:53:37 +0000

    Refactored string search to get input from standard input and command line arguments

--- 04-sorting-and-searching/NaiveSearch.cpp
+++ 04-sorting-and-searching/NaiveSearch.cpp
@@ -1,13 +1,17 @@
 #include <string>
+#include <map>
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <iterator>
+#include <vector>
+#include <cstdlib>
 
-#include "NaiveSearch.h"
-
-size_t Naive::find (const std::string& string, const std::string& substring, size_t pos) {
+long long find (const std::string& string, const std::string& substring, size_t pos = 0) {
 	size_t string_length = string.length();
 	size_t substring_length = substring.length();
 	bool match = false;
-	for (size_t i=0; i<string_length-substring_length+1; i++) {
+	for (size_t i=pos; i<string_length-substring_length+1; i++) {
 		for (size_t j=0; j<substring_length; j++) {
 			if (string.at(i+j) != substring.at(j)) {
 				break;
@@ -19,8 +23,35 @@ size_t Naive::find (const std::string& string, const std::string& substring, siz
 			}
 		}
 		if (match) {
-			return true;
+			return i;
 		}
 	}
-	return false;
+	return -1;
+}
+
+int main(int argc, char **argv) {
+	std::vector<std::string> args(argv, argv + argc);
+	size_t args_size = args.size();
+	size_t position = 0;
+
+	if (args_size < 2) {
+		std::cout << std::endl << "Usage: \e[1mnaive_search substring [position]\e[0m" << std::endl;
+		std::cout << "Searches standard input for \e[1msubstring\e[0m, optionally starting at \e[1mposition\e[0m index." << std::endl;
+		std::cout << "Returns index of \e[1msubstring\e[0m occurrence or -1 if not found." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (args_size > 2) {
+		position = std::atoi(args[2].c_str());
+	}
+
+	// don't skip the whitespace while reading
+	std::cin >> std::noskipws;
+
+	// use stream iterators to copy the stream to a string
+	std::istream_iterator<char> it(std::cin);
+	std::istream_iterator<char> end;
+	std::string string(it, end);
+
+	std::cout << find(string, args[1], position) << std::endl;
 }
--- 04-sorting-and-searching/RabinKarpSearch.cpp
+++ 04-sorting-and-searching/RabinKarpSearch.cpp
@@ -1,10 +1,14 @@
 #include <string>
 #include <map>
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <iterator>
+#include <vector>
+#include <cstdlib>
+
 #define ALPHABET_SIZE 127
 #define DEBUG 0
-
-#include "RabinKarpSearch.h"
 
 unsigned long rabin_karp(const std::string& string, size_t from = 0, size_t to = 0) {
 	#if DEBUG
@@ -43,7 +47,7 @@ unsigned long rabin_karp_incremental(const std::string& string, size_t from, siz
 	return hash;
 }
 
-size_t RabinKarp::find (const std::string& string, const std::string& substring, size_t pos) {
+long long find(const std::string& string, const std::string& substring, size_t pos = 0) {
 	size_t string_length = string.length();
 	size_t substring_length = substring.length();
 	#if DEBUG
@@ -52,7 +56,7 @@ size_t RabinKarp::find (const std::string& string, const std::string& substring,
 	unsigned long hash = rabin_karp(substring);
 	unsigned long candidate_hash = rabin_karp(string, pos, pos+substring_length);
 	if (candidate_hash == hash) {
-		return true;
+		return pos;
 	}
 	unsigned long precomputed_power_of_alpha = 1;
 	for (size_t i=0; i<substring_length-1; i++) {
@@ -64,8 +68,35 @@ size_t RabinKarp::find (const std::string& string, const std::string& substring,
 	for (size_t i=pos+1; i<string_length-substring_length+1; i++) {
 		candidate_hash = rabin_karp_incremental(string, i, i+substring_length, candidate_hash, precomputed_power_of_alpha);
 		if (candidate_hash == hash) {
-			return true;
+			return i;
 		}
 	}
-	return false;
+	return -1;
+}
+
+int main(int argc, char **argv) {
+	std::vector<std::string> args(argv, argv + argc);
+	size_t args_size = args.size();
+	size_t position = 0;
+
+	if (args_size < 2) {
+		std::cout << std::endl << "Usage: \e[1mrabin_karp_search substring [position]\e[0m" << std::endl;
+		std::cout << "Searches standard input for \e[1msubstring\e[0m, optionally starting at \e[1mposition\e[0m index." << std::endl;
+		std::cout << "Returns index of \e[1msubstring\e[0m occurrence or -1 if not found." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (args_size > 2) {
+		position = std::atoi(args[2].c_str());
+	}
+
+	// don't skip the whitespace while reading
+	std::cin >> std::noskipws;
+
+	// use stream iterators to copy the stream to a string
+	std::istream_iterator<char> it(std::cin);
+	std::istream_iterator<char> end;
+	std::string string(it, end);
+
+	std::cout << find(string, args[1], position) << std::endl;
 }
